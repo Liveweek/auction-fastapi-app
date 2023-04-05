1d473197567f5b87a411ebd1ec7029691be29e43
pokonakov (VTB70171591) <pokonakov@vtb.ru>
2023-04-05 18:58:47 +0300

    init objects

--- auction_project/app.py
+++ auction_project/app.py
@@ -34,44 +34,16 @@ def main():
     from sqlmodel import Session
     from enums import AuctionStatus
     create_db_and_tables()
-    # c = Category(
-    #     name="Шляпы",
-    #     description="Они всем как раз",
-    #     category_photo_path="/categories/ssss"
-    # )
-    # v = Vendor(
-    #     vendor_name="Алладин ебучий",
-    #     store_name="Магазин у Али Бабы",
-    #     store_phone_number="22-22-888",
-    #     store_site=r"http://www.google.com",
-    #     vendor_photo_path="/vendor/123123"
-    # )
-    # a = Auction(
-    #     lot_name="Шляпа султана",
-    #     lot_description="Ебаная шляпа султана",
-    #     lot_min_bet=11.5,
-    #     lot_status=AuctionStatus.auc_open,
-    #     category=c,
-    #     lot_vendor=v
-    # )
-    # b1 = Bet(
-    #     bet_datetime=datetime.datetime.now(),
-    #     bet_size=100.
-    # )
-    # b2 = Bet(
-    #     bet_datetime=datetime.datetime.now(),
-    #     bet_size=200.
-    # )
-    # a.auction_bets.extend([b1, b2])
     
-    user = User( **{"username": "johndoe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-    })
-    with Session(engine) as session:
-        session.add(user)
-        # session.add(a)
-        session.commit()
+    
+    # user = User( **{"username": "johndoe",
+    #     "email": "johndoe@example.com",
+    #     "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+    # })
+    # with Session(engine) as session:
+    #     session.add(user)
+    #     # session.add(a)
+    #     session.commit()
 
 if __name__ == "__main__":
     main()
--- auction_project/database.py
+++ auction_project/database.py
@@ -1,4 +1,8 @@
-from sqlmodel import SQLModel, create_engine
+import datetime
+from sqlmodel import SQLModel, Session, create_engine
+from auction_project.enums import AuctionStatus
+
+from auction_project.models.db_model import Auction, Bet, Category, Vendor
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -7,3 +11,39 @@ engine = create_engine(sqlite_url)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+    
+    c = Category(
+        name="Шляпы",
+        description="Они всем как раз",
+        category_photo_path="/categories/ssss"
+    )
+    v = Vendor(
+        vendor_name="Алладин ебучий",
+        store_name="Магазин у Али Бабы",
+        store_phone_number="22-22-888",
+        store_site=r"http://www.google.com",
+        vendor_photo_path="/vendor/123123"
+    )
+    a = Auction(
+        lot_name="Шляпа султана",
+        lot_description="Ебаная шляпа султана",
+        lot_min_bet=11.5,
+        lot_status=AuctionStatus.auc_open,
+        category=c,
+        lot_vendor=v
+    )
+    b1 = Bet(
+        bet_datetime=datetime.datetime.now(),
+        bet_size=100.
+    )
+    b2 = Bet(
+        bet_datetime=datetime.datetime.now(),
+        bet_size=200.
+    )
+    a.auction_bets.extend([b1, b2])
+    
+    with Session(engine) as session:
+        session.add(a)
+        session.commit()
+    
+    
